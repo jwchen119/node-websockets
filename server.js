9ff163aeba4643ffa4810983e0f83d3bafd42ee4
Matix200 <mateusz.mitek95@gmail.com>
2018-01-20 16:40:14 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const SocketServer = require('ws').Server;
 const path = require('path');
-cryptoSocket = require("crypto-socket")
+const cryptoSocket = require("crypto-socket")
 
 
 const PORT = process.env.PORT || 3000;
