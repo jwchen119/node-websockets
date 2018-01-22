f2bae8fc7cbc3009f37672734c740198ffbdc0f5
Matix200 <mateusz.mitek95@gmail.com>
2018-01-22 13:08:34 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const SocketServer = require('ws').Server;
 const path = require('path');
 const cryptoSocket = require("crypto-socket")
-
+cryptoSocket.start();
 
 const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
@@ -17,7 +17,6 @@ const wss = new SocketServer({ server });
 
 
 wss.on('connection', (ws) => {
-  cryptoSocket.start();
   console.log('Client connected');
   ws.on('close', () => console.log('Client disconnected'));
 });
