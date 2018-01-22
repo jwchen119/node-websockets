5b853ddb5aa7c226130968eba95217b3ad46c5b8
Matix200 <mateusz.mitek95@gmail.com>
2018-01-22 16:03:23 +0100

    Update server.js

--- server.js
+++ server.js
@@ -15,16 +15,25 @@ const server = express()
 
 const wss = new SocketServer({ server });
 
+wss.on('connection', function connection(ws, req) {
+  const location = url.parse(req.url, true);
+  // You might use location.query.access_token to authenticate or share sessions
+  // or req.headers.cookie (see http://stackoverflow.com/a/16395220/151312)
 
-wss.on('connection', (ws) => {
-  console.log('Client connected');
-  ws.on('close', () => console.log('Client disconnected'));
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+  });
+
+  ws.send('something');
 });
 
+//wss.on('connection', (ws) => {
+//  console.log('Client connected');
+//  ws.on('close', () => console.log('Client disconnected'));
+//});
 
-
-setInterval(() => {
-  wss.clients.forEach((client) => {
-      client.send(cryptoSocket.start("cex","ETHBTC"));
-  });
-}, 1000);
+//setInterval(() => {
+ // wss.clients.forEach((client) => {
+   //   client.send(cryptoSocket.start("cex","ETHBTC"));
+  //});
+//}, 1000);
