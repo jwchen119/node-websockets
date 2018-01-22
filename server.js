c3681bee7c783d499b4b980e198ffa14217bba68
Matix200 <mateusz.mitek95@gmail.com>
2018-01-22 16:33:17 +0100

    Update server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ var Crypto = "Suka blat";
 setTimeout(function() {
 setInterval(
 function(){
-console.log(JSON.stringify(cryptoSocket.start("bittrex","BTCXRP")));
+console.log(JSON.stringify(cryptoSocket.start("bittrex","ETHXRP")));
 },1000);
 }, 3000);
 
