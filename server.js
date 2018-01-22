e7f034d6682ccd9d493159d408caa15192d094de
Matix200 <mateusz.mitek95@gmail.com>
2018-01-22 16:26:28 +0100

    Update server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ var Crypto = "Suka blat";
 setTimeout(function() {
 setInterval(
 function(){
-console.log(JSON.stringify(cryptoSocket.start("cex","ETHBTC")));
+console.log(JSON.stringify(cryptoSocket.start("bitfinex",['LTCBTC','BTCUSD']));
 },1000);
 }, 3000);
 
