f0b888ac7158560c24a0158a05d3d677d40e61f4
jwchen119 <jwchen119@gmail.com>
2018-02-19 12:40:48 +0800

    Update server.js

--- server.js
+++ server.js
@@ -4,10 +4,17 @@ const express = require('express');
 const SocketServer = require('ws').Server;
 const path = require('path');
 //const coinTicker = require('coin-ticker');
-//const BFX = require('bitfinex-api-node')
-//const bfxRest = new BFX({ apiKey: '1234', apiSecret: '4321' }).rest
-const bfx = require('bfx')
-const rest = bfx.rest(2, { transform: true })
+const BFX = require('bitfinex-api-node')
+const bfx = new BFX({
+  apiKey: '1234',
+  apiSecret: '4321',
+
+  ws: {
+    autoReconnect: true,
+    seqAudit: true,
+    packetWDDelay: 10 * 1000
+  }
+})
 
 
 const PORT = process.env.PORT || 3000;
@@ -44,14 +51,14 @@ var Crypto = "請稍後...";
 //  })
 
 
-rest.ticker('ETHUSD', (err, res) => {
+bfx.ticker('tETHUSD', (err, res) => {
 	if (err) console.log(err)
 	console.log(res)
 })
 
 setInterval(() => {
     wss.clients.forEach((client) => {
-	rest.candles('1m', 'tBTCUSD', 'hist', (err, res) => {
+	bfx.candles('1m', 'tBTCUSD', 'hist', (err, res) => {
 	    if (err) console.log(err)
 	    console.log(res)
 	    Crypto = res
