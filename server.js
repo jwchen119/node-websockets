c4b4e1264b82dd85da2da3302748263f536c8250
jwchen119 <jwchen119@gmail.com>
2018-02-20 12:02:36 +0800

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const SocketServer = require('ws').Server;
 const path = require('path');
 const BFX = require('bitfinex-api-node');
 const bfx = new BFX();
-const bfxRest = new BFX('1234', '4321', {version: 2}).rest(2)
+const bfxRest = new BFX('1234', '4321', {version: 2})
 
 const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
